5dceafdc2a4efacc760bce5192a301a9028f2027
Guilherme Santana <guisantana0204@gmail.com>
2020-03-04 02:31:12 -0300

    Criacao da classe Conta, alguns exemplos de parametros, metodos

--- src/main/java/exemplos/Conta.java
+++ src/main/java/exemplos/Conta.java
@@ -6,12 +6,32 @@ public class Conta {
     double saldo;
 
     //Criando um metodo
-    void saca(double quantidade){
-        double novoSaldo = this.saldo - quantidade;
-        this.saldo = novoSaldo;
+    boolean saca(double valor){
+        if(this.saldo < valor){
+            return false;
+        }
+        else {
+            this.saldo = this.saldo - valor;
+            return true;
+        }
+
     }
 
     void deposita(double quantidade){
         this.saldo += quantidade;
+    }
+
+    boolean transfere(Conta destino, double valor){
+        this.saldo = this.saldo - valor;
+        destino.saldo = destino.saldo + valor;
+
+        boolean retirou = this.saca(valor);
+        if( retirou == false){
+            return false;
+        }
+        else {
+            destino.deposita(valor);
+            return true;
+        }
     }
 }

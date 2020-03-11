b80068d016713e12211d8a1e4755431286742aa0
Guilherme Santana <guisantana0204@gmail.com>
2020-03-11 02:28:32 -0300

    Criacao de classes para exemplos de encapsulamento, metodos getter e setter e construtores

--- src/main/java/exercicios/Conta.java
+++ src/main/java/exercicios/Conta.java
@@ -1,28 +1,56 @@
 package exercicios;
 
 public class Conta {
-    String nome;
+    private String nome;
     String agencia;
     int numero;
-    double saldo;
+    private double saldo;
     double rendimento;
     Data dataDeAbertura;
+    String titular;
 
-    void saca(double quantidade){
-        double novoSaldo = this.saldo - quantidade;
-        this.saldo = novoSaldo;
+    //Criando o construtor
+    Conta(){
+        System.out.println("-|Construindo uma conta|-");
+    }
+
+    /*Construtor com o titular da conta
+    Conta(String titular){
+        this.titular = titular;
+    }*/
+
+    //Setando valores nos atributos
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void saca(double quantidade){
+        if( quantidade > this.saldo){
+            System.out.println("Nao é possivel fazer a operacao, o valor solicitado e maior que o saldo disponivel");
+        } else {
+            this.saldo = this.saldo - quantidade;
+            System.out.println("Saque efetuado com sucesso . SALDO:" + this.saldo);
+        }
 
     }
 
-    void deposita(double quantidade){
-        this.saldo += quantidade;
+    public void deposita(double quantidade){
+        if(quantidade < 0){
+            System.out.println("Nao e possivel fazer o deposito");
+        } else {
+            this.saldo += quantidade;
+        }
+    }
+
+    public double pegaSaldo(){
+        return this.saldo;
     }
 
     void calculaRendimento(){
        this.rendimento = saldo * 0.1;
     }
 
-    String recuperarDadosParaImpressao(){
+   public  String recuperarDadosParaImpressao(){
         String dados = "Titular: " + this.nome;
         dados += "\nNumero: " + this.numero;
         dados += "\nAgencia: " + this.agencia;
@@ -32,6 +60,7 @@ public class Conta {
         return dados;
     }
 }
+
 
 
 
--- src/main/java/exercicios/TesteConta.java
+++ src/main/java/exercicios/TesteConta.java
@@ -4,14 +4,12 @@ public class TesteConta {
     public static void main(String[] args) {
         //criando uma nova conta
         Conta c1 = new Conta();
-        c1.nome = "Guilherme";
-        c1.saldo = 200;
+        c1.setNome("Guilherme");
         c1.agencia = "4567-7";
         c1.numero = 564;
 
         Conta c2 = new Conta();
-        c2.nome = "Guilherme";
-        c2.saldo = 200;
+        c2.setNome("Guilherme");
 
         Data d1 = new Data();
         c1.dataDeAbertura = d1;
@@ -19,6 +17,8 @@ public class TesteConta {
         d1.mes = 04;
         d1.ano = 2020;
 
+        c1.deposita(-30);
+        System.out.println("Saldo:" + c1.pegaSaldo());
 
         if( c1 == c2 ){
             System.out.println("Dados iguais das contas");
